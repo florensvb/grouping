acb173ed069e7db0d87f03a338ef746b063d3171
fbu <fb@kreatize.com>
2020-01-22 10:51:16 +0100

    creating groups and adding participants

--- contracts/Grouping.sol
+++ contracts/Grouping.sol
@@ -1,17 +1,33 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Grouping {
+    // the owner of the contract
     address public owner;
 
+    // a participant
     struct Participant {
         string name;
         address participantAddress;
     }
 
+    // number of groups
+    uint private groupCount = 0;
+
+    // a group
+    struct Group {
+        uint groupNumber;
+    }
+
+    // list of groups
+    Group[] groups;
+
+    // list of participants
+    Participant[] participants;
+
     // an address points to a participant
-    mapping (address => Participant) public participants;
+    mapping (address => Participant) public addressToParticipant;
     // an address points to a group
-    mapping (address => uint) public groups;
+    mapping (address => Group) public addressToGroup;
 
     // saving the owner of the contract
     constructor() public {
@@ -20,12 +36,33 @@ contract Grouping {
 
     // register new participants
     function register(string memory _name) public {
-        participants[msg.sender] = Participant(_name, msg.sender);
+        // create a new participant
+        Participant memory participant = Participant(_name, msg.sender);
+        // push it to the array
+        participants.push(participant);
+        // save it in the mapping
+        addressToParticipant[msg.sender] = participant;
+        // create a new group
+        Group memory group = createGroup();
+        // add participant to new group
+        addParticipantToGroup(participant, group);
     }
 
     // return a participant
     function getParticipant() public view returns (string memory name, address participantAddress) {
-        Participant memory participant = participants[msg.sender];
+        Participant memory participant = addressToParticipant[msg.sender];
         return (participant.name, participant.participantAddress);
+    }
+
+    // create a new group
+    function createGroup() private returns (Group memory) {
+        Group memory group = Group(groupCount++);
+        groups.push(group);
+        return group;
+    }
+
+    // add participant to group
+    function addParticipantToGroup(Participant memory participant, Group memory group) private {
+        addressToGroup[participant.participantAddress] = group;
     }
 }
